361fbb45d4ecf1f520e23fef7d788b563ab91f22
Manas Talukdar <manas.talukdar@gmail.com>
2019-09-15 01:27:26 -0700

    reorg files

--- .github/workflows/get-api-deps.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-go get -u github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway
-go get -u github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger
-go get -u github.com/golang/protobuf/protoc-gen-go
