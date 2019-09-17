8a8d9dc1e6ca8346d82af9e9d66b804562d709b1
Manas Talukdar <manas.talukdar@gmail.com>
2019-09-16 18:48:28 -0700

    added scripts

---
+++ scripts/get-api-deps.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+go get -u github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway
+go get -u github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger
+go get -u github.com/golang/protobuf/protoc-gen-go
